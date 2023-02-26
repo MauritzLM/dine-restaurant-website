794a7bca7f94ec1f6188f0aa810ccf3a000358b7
Mauritz Marais <mauritz303@gmail.com>
2023-02-26 15:40:53 +0200

    added validation to booking form

--- src/form.js
+++ src/form.js
@@ -1,8 +1,32 @@
 // form validation
+const form = document.querySelector(".booking-form");
+// form inputs
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const dateInput = document.querySelector("#date");
+const timeInput = document.querySelector("#time");
+
 // error message elements
 const emailError = document.querySelector(".email-error");
 const nameError = document.querySelector(".name-error");
 const dateError = document.querySelector(".date-error");
 const timeError = document.querySelector(".time-error");
 
-const form = document.querySelector(".booking-form");
+form.addEventListener('submit', (e) => {
+    // name missing
+    nameInput.validity.valueMissing ? nameError.textContent = 'This field is required' : nameError.textContent = '';
+
+    // email missing
+    emailInput.validity.valueMissing ? emailError.textContent = 'This field is required' : emailError.textContent = '';
+
+    // invalid email address
+    emailInput.validity.typeMismatch ? emailError.textContent = 'Please use a valid email address' : '';
+
+    // date missing
+    dateInput.validity.valueMissing ? dateError.textContent = 'This field is incomplete' : dateError.textContent = '';
+
+    // time missing
+    timeInput.validity.valueMissing ? timeError.textContent = 'This field is incomplete' : timeError.textContent = '';
+
+    e.preventDefault();
+});
