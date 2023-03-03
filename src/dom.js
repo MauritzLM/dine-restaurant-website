4c01fb0937711987c3475b58485eefef06eed9f5
Mauritz Marais <mauritz303@gmail.com>
2023-03-03 16:28:58 +0200

    added select class to events select with toggle, updated form styles

--- src/dom.js
+++ src/dom.js
@@ -2,10 +2,13 @@
 export function displayEventType(eventToDisplay, hideEvent1, hideEvent2) {
     eventToDisplay.image.className = `${eventToDisplay.name}-images`;
     eventToDisplay.text.className = `${eventToDisplay.name}-text`;
+    eventToDisplay.select.className = `${eventToDisplay.name}-select selected`;
 
     hideEvent1.image.className = `${hideEvent1.name}-images hidden`;
     hideEvent1.text.className = `${hideEvent1.name}-text hidden`;
+    hideEvent1.select.className = `${hideEvent1.name}-select`;
 
     hideEvent2.image.className = `${hideEvent2.name}-images hidden`;
     hideEvent2.text.className = `${hideEvent2.name}-text hidden`;
+    hideEvent2.select.className = `${hideEvent2.name}-select`;
 };
--- src/index.js
+++ src/index.js
@@ -7,16 +7,19 @@ const eventsContainer = document.querySelector(".events");
 const familyEvents = { name: "family" };
 familyEvents.image = document.querySelector(".family-images");
 familyEvents.text = document.querySelector(".family-text");
+familyEvents.select = document.querySelector(".family-select");
 
 // special elements
 const specialEvents = { name: "special" };
 specialEvents.image = document.querySelector(".special-images");
 specialEvents.text = document.querySelector(".special-text");
+specialEvents.select = document.querySelector(".special-select");
 
-//social elements
+// social elements
 const socialEvents = { name: "social" };
 socialEvents.image = document.querySelector(".social-images");
 socialEvents.text = document.querySelector(".social-text");
+socialEvents.select = document.querySelector(".social-select");
 
 // click event to toggle event type (using hidden class) 
 eventsContainer.addEventListener("click", (e) => {
